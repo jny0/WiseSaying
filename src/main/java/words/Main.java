e9544646d1bd4a8711792d0d9381fd330009b51b
jny0 <pjy100402@naver.com>
2023-03-02 09:30:48 +0900

    step3

--- src/main/java/words/Main.java
+++ src/main/java/words/Main.java
@@ -7,11 +7,23 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("==명언 앱==");
-        System.out.print("명령) ");
-        String input = sc.nextLine();
 
-        if(input =="종료"){
+        int i = 1;
+        while( i>0 ){
+            String input = sc.nextLine();
+            System.out.print("명령) ");
 
+            if(input.equals("등록")){
+                System.out.print("명언 : ");
+                String 명언 = sc.nextLine();
+
+                System.out.print("작가 : ");
+                String 작가 = sc.nextLine();
+                System.out.println(i + "번 명언이 등록되었습니다.");
+                i++;
+            } else if(input.equals("종료")){
+                break;
+            }
         }
 
         sc.close();
